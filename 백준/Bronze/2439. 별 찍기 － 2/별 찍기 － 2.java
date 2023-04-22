dea0657727a70c1c6d3b1881413a66d9b0d192fa
박태석 <107897812+TeTedo@users.noreply.github.com>
2023-04-23 00:34:27 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 148 ms, Memory: 15100 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        try(
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ){
+            int count = Integer.parseInt(br.readLine());
+            for(int i = 0; i < count; i++){
+                String star = "";
+                for(int j = count - 1; j > i; j--){
+                    star += " ";
+                }
+                
+                for(int j = 0; j <= i; j++){
+                    star += "*";
+                }
+                bw.write(star + "\n");
+            }
+            bw.flush();
+        }
+    }
+}
