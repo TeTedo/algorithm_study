5befeb83805078a9c9580662e7c08c8d360671e7
박태석 <qkrxotjr9508@gmail.com>
2023-07-06 23:51:44 +0900

    [level 3] Title: 등굣길, Time: 0.65 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 1;
+        int[] dx = {0,1};
+        int[] dy = {1,0};
+        
+        int[][] visited = new int[n+1][m+1];
+        for(int i = 0; i < puddles.length; i++) {
+            visited[puddles[i][1]][puddles[i][0]] = 1;
+        }
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{1,1});
+        
+        int[][] dp = new int[n+1][m+1];
+        dp[1][1] = 1;
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 1; j < dp[i].length; j++) {
+                if(i == 1 && j == 1) continue;
+                if(visited[i][j] == 1) continue;
+                dp[i][j] = dp[i][j-1]%1000000007 + dp[i-1][j]%1000000007;
+            }
+        }
+        return dp[n][m]%1000000007;
+    }
+}
