caa63ea1151e2dc3a897b3f59981edd22f9d9bb9
박태석 <qkrxotjr9508@gmail.com>
2024-11-08 12:26:41 +0900

    백준 자바 템플릿

---
+++ untitled/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        try(
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int book = Integer.parseInt(st.nextToken());
+            int problem = Integer.parseInt(st.nextToken());
+
+            HashMap<String, String> changeMap = new HashMap<>();
+
+            for(int i = 0; i < book; i++) {
+                String poketmon = br.readLine();
+                changeMap.put(poketmon, Integer.toString(i+1));
+                changeMap.put(Integer.toString(i+1), poketmon);
+            }
+
+            for(int i = 0; i < problem; i++) {
+                String str = br.readLine();
+                System.out.println(changeMap.get(str));
+            }
+        }
+    }
+}
